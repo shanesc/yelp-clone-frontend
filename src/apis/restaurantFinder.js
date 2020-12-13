3ece15006610d2c00ed131397ff783d705ce3648
Shane Mitchell <shaneSC@gmail.com>
2020-12-13 15:10:27 +0000

    Update API url

--- src/apis/restaurantFinder.js
+++ src/apis/restaurantFinder.js
@@ -1,5 +1,7 @@
 export const url = {
-  // 'https://safe-basin-83132.herokuapp.com/api/v1/restaurants/';
-  restaurants: 'http://localhost:3001/api/v1/restaurants/',
-  users: 'http://localhost:3001/api/v1/users/',
+  restaurants:
+    'https://safe-basin-83132.herokuapp.com/api/v1/restaurants/',
+  users: 'https://safe-basin-83132.herokuapp.com/api/v1/users/',
+  // restaurants: 'http://localhost:3001/api/v1/restaurants/',
+  // users: 'http://localhost:3001/api/v1/users/',
 };
