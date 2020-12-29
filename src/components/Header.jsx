9a3c6e61678cd7334076c950de28accd4764873b
Shane Mitchell <shaneSC@gmail.com>
2020-12-29 15:34:49 +0000

    Add .eslintcache to .gitignore

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div>
-      <h1 className="font-weigth-light display-1 text-center">
+      <h1 className="font-weight-light display-1 text-center">
         Restaurant Finder
       </h1>
     </div>
